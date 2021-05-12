c99f262c3fc6e5ffa26b36846364bae0cb8f35f9
Sean Mulder <sean@Erins-Air.lan>
2021-05-11 20:52:26 -0400

    Refactored config stuff

---
+++ config.py
@@ -0,0 +1,40 @@
+high = { "STR" : "strong",
+         "DEX" : "agile",
+         "CON" : "tough",
+         "INT" : "smart",
+         "WIS" : "wise",
+         "CHA" : "persuasive" }
+low = { "STR" : "weak",
+         "DEX" : "slooow",
+         "CON" : "fragile",
+         "INT" : "dumb",
+         "WIS" : "foolish",
+         "CHA" : "homely" }
+profession = { "STR" : "Fighter",
+         "DEX" : "Ranger",
+         "CON" : "Barbarian",
+         "INT" : "Wizard",
+         "WIS" : "Cleric",
+         "CHA" : "Bard" }
+quirk1 = ["is scared of", "loves", "hates", "is allergic to", "has an addiction to", "is a collecter of", "is unfortunately fond of"]
+quirk2 = ["pizza", "love", "onions", "the cold", "turkeys", "caves", "baths", "clerics", "fruit bats", "swords", "wood", "paper", "bows", "hostages", "medals", "awards", "rubies", "gods", "phones", "ipads", "ale", "fine wine", "boats", "treehouses"]
+cantripsoffense = ["Fireball", "Ice Shard", "Lightning Bolt", "Earth Chunk", "Wind Blade"]
+cantripsdefense = ["Fire Wall", "Ice Shield", "Lightning Deflect", "Earth Armor", "Wind Gust"]
+cantripsutil = ["Fire Light", "Ice Path", "Lightning Field", "Earth Mold", "Wind Push"]
+weaponsfighter = ["Greatsword", "Greataxe"]
+weaponsranger = ["Longbow", "Crossbow"]
+weaponsbarbarian = ["Warhammer", "Battleaxe"]
+instrumentsbard = ["Violin", "Viola", "Cello", "Lute", "Mandola"]
+
+races = ["Dragonborn", "Elf", "Dwarf", "Human", "Halfling", "Half-Orc", "Gnome", "Tiefling"]
+
+racial_modifiers = {
+"Dwarf" : {"CON":2},
+"Elf" : {"DEX":2},
+"Halfling" : {"DEX":2},
+"Human" : {"STR":1, "DEX":1, "CON":1, "INT":1, "WIS":1, "CHA":1},
+"Dragonborn" : {"STR":2, "CHA":1},
+"Gnome" : {"INT":2},
+"Half-Orc" : {"STR":2, "CON":1},
+"Tiefling" : {"INT":1,"CHA":2}
+}
