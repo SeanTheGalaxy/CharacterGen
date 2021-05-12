2d455b6e60f4bd311761ec29640a29f7ea9ab6a9
Sean Mulder <sean@Erins-Air.lan>
2021-05-11 22:45:08 -0400

    Refactored to separate model and view, added file storage

--- character.py
+++ character.py
@@ -1,5 +1,7 @@
+import json
 import random
 import config
+import os
 
 high = config.high
 low = config.low
@@ -16,10 +18,107 @@ instrumentsbard = config.instrumentsbard
 racial_modifiers = config.racial_modifiers
 races = config.races
 
+
+# Takes a dictionary with a list of characters and saves to storage
+def write_characters(data):
+    with open('characters.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+# Loads characters from storage and returns a dictionary with a list of characters
+def load_characters():
+    if os.path.exists('characters.json') :
+        with open('characters.json') as json_file:
+            data = json.load(json_file)
+            return data
+    else :
+        return {"characters":[]}
+
+# Saves a single character (dictionary)
+def save_character(character):
+    data = load_characters()
+    data["characters"].append(character)
+    write_characters(data)
+
+def rollattribute():
+    roll0a = [random.randint(1,6), random.randint(1,6), random.randint(1,6), random.randint(1,6)]
+    roll0a = sorted(roll0a)
+    del roll0a[0]
+    roll0 = sum(roll0a)
+    return roll0
+
+
+def generate_stats(race):
+    stats = dict()
+    stats["STR"] = rollattribute()
+    stats["DEX"] = rollattribute()
+    stats["CON"] = rollattribute()
+    stats["INT"] = rollattribute()
+    stats["WIS"] = rollattribute()
+    stats["CHA"] = rollattribute()
+
+    # apply racial modifiers
+    for stat in stats:
+        if stat in racial_modifiers[race] :
+            stats[stat] = stats[stat]+racial_modifiers[race][stat]
+
+    return stats
+
+
+def generate_character(name, pronoun, race, pcclass, stats):
+    pc = {}
+
+    pc["name"] = name
+    pc["race"] = race
+    pc["pcclass"] = pcclass
+    pc["stats"] = stats
+    pc["pronoun"] = pronoun
+
+    # generate quirks
+    pc["quirks"] = []
+    for i in range(3):
+        pc["quirks"].append(random.choice(quirk1)+ " " + random.choice(quirk2))
+
+    # generate spells
+    pc["spells"] = []
+    if pcclass in ["Wizard", "Cleric", "Bard"]:
+        pc["spells"].append(random.choice(cantripsoffense))
+        pc["spells"].append(random.choice(cantripsdefense))
+
+    if pcclass in ["Wizard", "Bard"]:
+        pc["spells"].append(random.choice(cantripsutil))
+    elif pcclass == "Cleric":
+        pc["spells"].append("Heal")
+
+    # generate weapons
+
+    pc["weapons"] = []
+    if pcclass == "Fighter":
+        pc["weapons"].append(random.choice(weaponsfighter))
+    elif pcclass == "Ranger":
+        pc["weapons"].append(random.choice(weaponsranger))
+    elif pcclass == "Barbarian":
+        pc["weapons"].append(random.choice(weaponsbarbarian))
+    elif pcclass in ["Wizard", "Cleric"]:
+        pc["weapons"].append("Staff")
+        pc["weapons"].append("Dagger")
+    elif pcclass == "Bard":
+        pc["weapons"].append("Dagger")
+
+    # generate gear
+
+    pc["gear"]=[]
+
+    if pcclass == "Bard":
+        pc["gear"].append(random.choice(instrumentsbard))
+    return pc
+
+
+
 name = input("What Shall We Name Your Character? ")
-name = name.lower().capitalize()
+name= name.lower().capitalize()
 pronoun = input("What is their pronoun? ")
 print("Ok", pronoun, "Shall Be Called", name)
+pronoun = pronoun.lower()
 himherthem = pronoun
 if pronoun == "she":
     himherthem = "her"
@@ -31,38 +130,19 @@ else:
     himherthem = name
 print("And all shall worship", himherthem + ".", "For", pronoun, "is a...")
 
-race = races[random.randint(0,len(racelist)-1)]
+race = random.choice(races)
 print(race)
 
 #def rollstat():
     #return random.randint(3, 20)
 
-def rollattribute():
-    roll0a = [random.randint(1,6), random.randint(1,6), random.randint(1,6), random.randint(1,6)]
-    roll0a = sorted(roll0a)
-    del roll0a[0]
-    roll0 = sum(roll0a)
-    return roll0
 
 
-# roll stats
-stats = dict()
-stats["STR"] = rollattribute()
-stats["DEX"] = rollattribute()
-stats["CON"] = rollattribute()
-stats["INT"] = rollattribute()
-stats["WIS"] = rollattribute()
-stats["CHA"] = rollattribute()
-
-# apply racial modifiers
-for stat in stats:
-    if stat in racial_modifiers[race] :
-        stats[stat] = stats[stat]+racial_modifiers[race][stat]
-
+stats = generate_stats(race)
 adjectives = []
 suggestions = []
 for stat in stats :
-    #print(stat+ ":", stats[stat])
+    #print(stat+ ":", pc["stats"][stat])
     if stats[stat] < 9:
         adjectives.append(low[stat])
     elif stats[stat] > 13:
@@ -82,34 +162,41 @@ print("and",pronoun,"would make a good", smartjoin(suggestions, "or", "hostage")
 pcclass = input("What do you want to be? ")
 pcclass = pcclass.lower().capitalize()
 
+
+pc = generate_character(name=name, race=race, pcclass=pcclass, pronoun=pronoun, stats=stats)
+
+
 print("\n\n")
 print("--------------------------------")
-print("Name:", name)
-print("Class:", pcclass)
-print("Race:", race)
+print("Name:", pc["name"])
+print("Class:", pc["pcclass"])
+print("Race:", pc["race"])
+
 print("")
-for stat in stats :
-    print(stat+ ":", stats[stat])
+for stat in pc["stats"] :
+    print(stat+ ":", pc["stats"][stat])
+
+print("")
 print("Quirks:")
-for i in range(3):
-    print(quirk1[random.randint(0,len(quirk1)-1)], quirk2[random.randint(0,len(quirk2)-1)])
-if pcclass == "Wizard" or pcclass == "Cleric" or pcclass == "Bard":
+for q in pc["quirks"] : print("   " + q)
+
+if len(pc["spells"]) > 0 :
+    print("")
     print("Spells:")
-    print(cantripsoffense[random.randint(0,len(cantripsoffense)-1)])
-    print(cantripsdefense[random.randint(0,len(cantripsdefense)-1)])
-if pcclass == "Wizard" or pcclass == "Bard":
-    print(cantripsutil[random.randint(0,len(cantripsutil)-1)])
-if pcclass == "Cleric":
-    print("Heal")
-print("Weapons")
-if pcclass == "Fighter":
-    print(weaponsfighter[random.randint(0,len(weaponsfighter)-1)])
-elif pcclass == "Ranger":
-    print(weaponsranger[random.randint(0,len(weaponsranger)-1)])
-elif pcclass == "Barbarian":
-    print(weaponsbarbarian[random.randint(0,len(weaponsbarbarian)-1)])
-elif pcclass == "Wizard" or pcclass == "Cleric":
-    print("Staff, Dagger")
-elif pcclass == "Bard":
-    print(instrumentsbard[random.randint(0,len(instrumentsbard)-1)]+", Dagger")
+    for s in pc["spells"]: print("   " + s)
+
+if len(pc["weapons"]) > 0 :
+    print("")
+    print("Weapons:")
+    for w in pc["weapons"]: print("   " + w)
+
+if len(pc["gear"]) > 0 :
+    print("")
+    print("Gear:")
+    for g in pc["gear"]: print("   " + g)
 print("--------------------------------")
+if input("Do you want to save " + name + "? (Y/N) ").lower() == "y":
+    save_character(pc)
+    print("Saved", name+"!")
+else:
+    print(name, "is gone forever. Wah Wah Waaaaaaaah. :(")
